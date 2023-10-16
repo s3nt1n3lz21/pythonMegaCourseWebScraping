9b980514a96ed233b41aa501f1ede0057ac32c4d
s3nt1n3lz21 <s3nt1n3lz18@gmail.com>
2023-10-16 17:37:13 +0100

    Now scraping data and storing it in a text file

---
+++ main.py
@@ -0,0 +1,39 @@
+import requests
+import selectorlib
+
+URL = "https://programmer100.pythonanywhere.com/tours/"
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+def scrape(url):
+    response = requests.get(url, headers=HEADERS)
+    source = response.text
+    return source
+
+def extract(source):
+    extractor = selectorlib.Extractor.from_yaml_file("extract.yaml")
+    value = extractor.extract(source)["tours"]
+    return value
+
+def send_email():
+    print("Email was sent!")
+def store(extracted):
+    with open("data.txt", "a") as file:
+        file.write(extracted + "\n")
+
+def read():
+    try:
+        with open("data.txt", "r") as file:
+            return file.read()
+    except FileNotFoundError:
+        with open("data.txt", "w") as file:
+            return ""
+
+if __name__ == '__main__':
+    scraped = scrape(URL)
+    extracted = extract(scraped)
+    print(extracted)
+    if extracted != "No upcoming tours":
+        content = read()
+        if extracted not in content:
+            store(extracted)
+            send_email()
